837c7ce2439a5916c085ca8ab2e20c8eb5273347
白日蓝梦 <306006128@qq.com>
2024-08-16 16:45:11 +0800

    第三次

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 
 <template>
+  
   <router-view></router-view>
   <!-- 增加一段注释 -->
+   
 </template>
 <script lang="ts" setup >
 // 更改
 </script>
 <style scoped>
-
+.clone{
+  font: 1em sans-serif;
+}
 </style>
